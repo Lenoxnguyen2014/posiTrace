4a13bdc529236564035256f81de1c3e7c08b7c82
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-07-27 12:57:44 -0700

    test

--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Geolocation, type: :model do
-  
-  let (:test_geolocation) {create(:geolocation)}
-  describe "geolocation is valid with ipv4 " do
-      it "all variables are presence " do
-        
+    describe "geolocation is valid with ipv4 " do
+      let (:test_geolocation) {create(:geolocation)}
 
+      it "all variables are presence " do
         expect(test_geolocation).to be_valid
       end
-    
+      it { is_expected.to validate_presence_of :ip }
   end
-  describe "geolocation is not valid with ipv6" do
-  end
+  # describe "geolocation is not valid with ipv6" do
+  #   let (:test_geolocation) {create(:geolocation, ip: Faker::Internet.ip_v6_address)}
+  #   it "return []" do
+  #     puts test_geolocation.ip
+      
+  #   end
 
-  describe "ip is a must, return empty if no ip or not valid ip" do
-  end
+  # end
+
+  # describe "ip is a must, return empty if no ip or not valid ip" do
+  # end
 
 
 end
