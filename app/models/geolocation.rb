926c817262e8c095d876a30deb66408538f331c4
lenoxnguyen2014 <lenoxnguyen2014@gmail.com>
2024-07-27 14:47:39 -0700

    clean up docker

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,4 +1,3 @@
-require 'Resolv'
 
 class Geolocation < ApplicationRecord
     validates_presence_of :ip, :typeip, :continent_code, :continent_name, :country_code, :region_code, :region_name, :city, :zip, :latitude, :longitude, :location
